c5bc747e4facbd2fb3cd13e7eb7824ed4d19355a
Sane Aalam <saneraza78692@gmail.com>
2024-03-15 15:51:01 +0530

    first commit

---
+++ March 15,2024 - Work Report Update/WebPage-Design.js
@@ -0,0 +1,13 @@
+// When we click button then - show the chat box
+// when we click close Then - hide the chat box
+
+// open from
+function ButtonClickOpenFrom() {
+  document.getElementById("ShowTheFromContainer").style.display = "block";
+  console.log("hello open from button");
+}
+
+// close from
+function ButtonClickCloseFrom() {
+  document.getElementById("ShowTheFromContainer").style.display = "none";
+}
